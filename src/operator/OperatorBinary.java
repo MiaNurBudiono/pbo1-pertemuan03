47696eab4747ab0fd18809339375d18ef026d830
Mia <mianurbudionoxiapkb@gmail.com>
2020-04-30 14:11:04 +0700

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -4,5 +4,14 @@ public class OperatorBinary {
     String nama = "Mia Nur Budiono";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.35;
-    
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+
 }
